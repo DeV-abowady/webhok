b19881c8a2de919bfa10047b1ac9eecf1593ad26
DeV-abowady <107771946+DeV-abowady@users.noreply.github.com>
2023-05-20 19:14:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from flask import Flask, render_template
+from threading import Thread
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+  render_template('index.html')
+
+
+if __name__ == "__main__":
+  bot.polling()
+  app.run(host='0.0.0.0')
